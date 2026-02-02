1ceaa1577ed513b7c96f89ada4cc17887922ddf8
Prasanth Selvamurugan <prasanthselvaads@gmail.com>
2026-02-02 23:04:20 +0530

    Security: Remove sensitive generation scripts

--- backend/gen_bcrypt.js
+++
@@ -1,7 +0,0 @@
-const bcrypt = require('bcryptjs');
-const fs = require('fs');
-
-const password = '04@06!!07@!!0';
-const hash = bcrypt.hashSync(password, 10);
-console.log('BCRYPT_HASH=' + hash);
-fs.writeFileSync('temp_bcrypt_hash.txt', hash);
