02637c926ecaade3a6e38694aaa5e91838653f9f
Prasanth Selvamurugan <prasanthselvaads@gmail.com>
2026-02-09 09:11:42 +0530

    Updated announcement poster as postermodel.tsx

--- components/PosterModal.tsx
+++ components/PosterModal.tsx
@@ -68,7 +68,7 @@ const PosterModal: React.FC = () => {
                     {/* Poster Image - using object-cover to 'crop' */}
                     <div className="w-full h-full">
                         <img
-                            src="/public/Announcement.jpeg"
+                            src="/Announcement.jpeg"
                             alt="Impact AI Thon Poster"
                             className="w-full h-full object-cover"
                             onError={(e) => {
